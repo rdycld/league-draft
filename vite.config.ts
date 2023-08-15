4234282559b014d61d53a13d6353695a8c801e08
Bartosz Ciach <bartosz.ciach@tooploox.com>
2023-08-15 22:06:37 +0200

    add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), vanillaExtractPlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      components: `${path.resolve(__dirname, './src/components')}`,
+      utils: `${path.resolve(__dirname, './src/utils')}`,
+      pages: path.resolve(__dirname, './src/pages'),
+      styles: path.resolve(__dirname, './src/styles'),
+    },
+  },
+});
